f81c00773eb8879907b4ff38fa5fb9b5c8e13d0e
Luis Guerrero <160375244+luishei@users.noreply.github.com>
2025-07-10 15:14:12 -0500

    Agregando código inicial

--- contracts/Votacion.sol
+++ contracts/Votacion.sol
@@ -1,1 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+contract Votacion {
+    struct Voto {
+        address votante;
+        uint candidato;
+    }
+
+    Voto[] public votos;
+    mapping(address => bool) public haVotado;
+
+    function votar(uint _candidato) public {
+        require(!haVotado[msg.sender], "Ya votaste.");
+        votos.push(Voto(msg.sender, _candidato));
+        haVotado[msg.sender] = true;
+    }
+
+    function totalVotos() public view returns (uint) {
+        return votos.length;
+    }
+}
